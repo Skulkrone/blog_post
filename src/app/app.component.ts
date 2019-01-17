979c257addef4328ead667225ad3364caf85ac7e
Laurent BARON <lbaron@mbp-de-laurent.home>
2019-01-17 12:45:33 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'posts';
+export class AppComponent implements OnInit {
+
+  posts = [
+    {
+      title: 'Mon premier post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque auctor ut velit at gravida. Aliquam non commodo ipsum. Nunc convallis sodales gravida.',
+      loveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: 'Mon deuxième post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque auctor ut velit at gravida. Aliquam non commodo ipsum. Nunc convallis sodales gravida.',
+      loveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: 'Encore un post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque auctor ut velit at gravida. Aliquam non commodo ipsum. Nunc convallis sodales gravida. Pellentesque lorem diam, dictum vitae condimentum a, mattis vel neque. Ut a ultrices sapien. Nulla a metus id purus egestas fringilla.',
+      loveIts: 0,
+      created_at: new Date()
+    }
+  ];
+
+   constructor() {
+   }
+
+   ngOnInit() {
+   }
 }
---
+++ src/app/post-list-item/post-list-item.component.ts
@@ -0,0 +1,33 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+
+@Component({
+  selector: 'app-post-list-item',
+  templateUrl: './post-list-item.component.html',
+  styleUrls: ['./post-list-item.component.scss']
+})
+export class PostListItemComponent implements OnInit {
+
+
+  @Input() postTitle: string;
+  @Input() postContent: string;
+  @Input() postLoveIts: number;
+  @Input() postCreatedAt: string;
+
+
+  onLoveIts() {
+    this.postLoveIts++;
+  }
+
+  offLoveIts() {
+    this.postLoveIts--;
+  }
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
